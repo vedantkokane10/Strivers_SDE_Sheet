9923f271c1e93036dbb74f334f3508e6ce1e14ff
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2024-08-11 22:14:02 +0530

    Update 01_Set_Matrix_Zeros.cpp

--- 01_Arrays_1/01_Set_Matrix_Zeros.cpp
+++ 01_Arrays_1/01_Set_Matrix_Zeros.cpp
@@ -1,3 +1,9 @@
+/*
+Assume that the first row and column are seperate row and column which will be marked if any 0 is found in ith row or jth column.
+Then just iterate over the matrix again if first row or column of the current element is 0 then mark the current cell as 0.
+We need to keep two boolean variable rowZero and colZero to check for 0th row and 0th column, if either of both is set then update the first row and column with 0s.
+*/
+
 void setZeroes(vector<vector<int>>& matrix) {
     int m = matrix.size();
     int n = matrix[0].size();
