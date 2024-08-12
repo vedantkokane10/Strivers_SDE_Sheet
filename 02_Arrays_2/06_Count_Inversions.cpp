9c81fd23d8ec9cd292ac79a4ba84d9dec0a9a783
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2024-08-13 00:21:26 +0530

    Create 06_Count_Inversions.cpp

---
+++ 02_Arrays_2/06_Count_Inversions.cpp
@@ -0,0 +1,70 @@
+/*
+Given an array of integers. Find the Inversion Count in the array.  Two array elements arr[i] and arr[j] form an inversion if arr[i] > arr[j] and i < j.
+Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If the array is already sorted then the inversion count is 0.
+If an array is sorted in the reverse order then the inversion count is the maximum. 
+
+
+We know  that in merge sort we have 2 sorted arrays before merging,
+so if we have 2 arrays [2,4] and [1,3,5]
+
+while merging 2 arrays, using i,j pointers 
+if leftArray[i] > rightArray[j]
+then all the elements from 0 to j in rightArray are also less than leftArray[i] so that pairs also count
+so we update the answer variable with the length of rightArray ((mid - low + 1) - left).
+
+For instance for 4 in leftArray the j will point to 3 in rightArray, so 4 can form with all elements before 3 (like (4,1)) and with 3. 
+*/
+
+long long ans = 0;
+
+void merge(long long arr[],long long low,long long mid,long long high){
+	vector<long long > leftArray,rightArray;
+	for(int i=low;i<=mid;i++){
+		leftArray.push_back(arr[i]);
+	}
+	for(int i=mid+1;i<=high;i++){
+		rightArray.push_back(arr[i]);
+	}
+	long long  left = 0;
+	long long  right = 0;
+	long long  k = low;
+	while(left < leftArray.size() && right < rightArray.size()){
+		if(leftArray[left] <= rightArray[right]){
+			arr[k] = leftArray[left];
+			left++;
+		}
+		else{
+		  ans += (mid - low + 1) - left;
+			arr[k] = rightArray[right];
+			right++;
+		}
+		k++;
+	}
+	while(left < leftArray.size()){
+		arr[k] = leftArray[left];
+		left++;
+		k++;
+	}
+	while(right < rightArray.size()){
+		arr[k] = rightArray[right];
+		right++;
+		k++;
+	}
+}
+
+void mergeSort(long long arr[],long long low,long long high){
+	if(low >= high){
+		return;
+	}
+	int mid = (low+high)/2;
+	mergeSort(arr,low,mid);
+	mergeSort(arr,mid+1,high);
+	merge(arr,low,mid,high);
+}
+
+long long int inversionCount(long long arr[], int n) {
+    // Your Code Here
+    
+    mergeSort(arr,0,n-1);
+    return ans;
+}
