ad83186169219bd1ac01489b335fbe412eabf343
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2024-08-13 19:14:45 +0530

    Update 04_Remove_Nth_Node_from_behind.cpp

--- 05_LinkedList_01/04_Remove_Nth_Node_from_behind.cpp
+++ 05_LinkedList_01/04_Remove_Nth_Node_from_behind.cpp
@@ -1,3 +1,11 @@
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Ex- 
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+*/
+
 ListNode* removeNthFromEnd(ListNode* head, int n) {
     int nodesCnt = 1;
     ListNode* temp = head;
